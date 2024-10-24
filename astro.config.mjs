29033d8577ea9c05fb6534f49bd623505e889a5f
Scrrrr <Scrrrr@users.noreply.github.com>
2024-10-24 17:56:27 +0900

    config astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://scrrrr.github.io",
+  site: 'https://scrrrr.github.io',
+  base: '/',
   integrations: [preact()]
 });
