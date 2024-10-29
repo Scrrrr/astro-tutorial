c2a366070c281561dba592a10960e5370554809a
Scrrrr <Scrrrr@users.noreply.github.com>
2024-10-29 09:52:51 +0900

    deployされない理由がシャドバンだったことがわかる

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  output: 'server',
-  adapter: cloudflare({ mode: "directory"})
+  site: 'https://scrrrr.github.io',
 });
